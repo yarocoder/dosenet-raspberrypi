a3deb56c43c78378bb66c046f1b4897d7f043d98
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-08-04 09:43:57 -0700

    Axes list indexing error debugging

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -437,7 +437,7 @@ class Real_Time_Spectra(object):
         '''
         # if self.spectrum_drawn == False:
 
-            self.sum_graph(avg_data, sum_data)
+        self.sum_graph(avg_data, sum_data)
 
             # plt.figure(2).canvas.draw()
 
@@ -447,7 +447,7 @@ class Real_Time_Spectra(object):
 
             # plt.draw()
 
-            self.spectrum_drawn = True
+            # self.spectrum_drawn = True
 
         # elif self.spectrum_drawn == True:
 
@@ -457,7 +457,7 @@ class Real_Time_Spectra(object):
             #
             # plt.figure(2).canvas.restore_region(self.spectrum_background)
             #
-            print(plt.figure(2).axes)
+        print(plt.figure(2).axes)
             #
             # # plt.figure(2).axes[0].draw_artist(self.spectrum_plot[0])
             #
@@ -468,12 +468,12 @@ class Real_Time_Spectra(object):
             '''
             Update the plot with the new spectrum.
             '''
-            plt.figure(2).canvas.update()
+        plt.figure(2).canvas.update()
 
             '''
             Refresh the Qt events used to create the canvas.
             '''
-            plt.figure(2).canvas.flush_events()
+        plt.figure(2).canvas.flush_events()
 
         # '''
         # Show the spectrum plot.
