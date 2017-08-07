9dc14f155fadf5d8b5307912e1520e65c13f0493
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-08-07 14:30:25 -0700

    Colorbar adding debugging

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -503,7 +503,7 @@ class Real_Time_Spectra(object):
         '''
         Add a colorbar.
         '''
-        plt.figure(1).colorbar()
+        plt.figure(1).canvas.colorbar()
         # plt.colorbar()
 
         '''
